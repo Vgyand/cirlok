814192ec4327b13c5af87d6eb859cf646f7128dd
Vgyand <gyandzhunczevv@gmail.com>
2022-10-11 11:08:39 +0300

    mobile

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,9 +1,18 @@
 const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("nav");
+const menuBtnDesk = document.getElementById("menuBtnDesk");
 
 let open = false;
 
 menuBtn.addEventListener("click", () => {
+  open = !open;
+  if (open) {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+});
+menuBtnDesk.addEventListener("click", () => {
   open = !open;
   if (open) {
     menu.style.display = "block";
